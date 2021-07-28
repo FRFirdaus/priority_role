6f99c065e3e99d0003dffa4a9728cc2114a2bc50
REHAN LC <frayhands@gmail.com>
2021-07-28 12:41:10 +0700

    add logic to check space on NIK

--- models/karyawan.py
+++ models/karyawan.py
@@ -31,14 +31,16 @@ class Karyawan(models.Model):
 
     role_id = fields.Many2one('karyawan.role')
 
-
     @api.constrains('nik')
     def get_join_date_from_nik(self):
         for rec in self:
             nik = rec.nik
-            date = "%s-%s-01" % (nik[8:12], nik[12:14])
+            clean_nik = nik.replace(" ", "")
+            if len(clean_nik) < 18:
+                raise ValidationError(_("Jumlah karakter pada NIK tidak boleh kurang dari 18 karakter, jumlah karakter pada NIK: %s" % (len(clean_nik))))
+            
+            date = "%s-%s-01" % (clean_nik[8:12], clean_nik[12:14])
             rec.join_date = date
-
 
 class KaryawanRole(models.Model):
     _name = 'karyawan.role'
